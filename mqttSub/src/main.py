fcf5c9d0d7e25752760e7245588d9f83bd1440a8
Matthias Morath <matthiasmorath@googlemail.com>
2023-11-05 23:45:17 +0100

    modified:   docker-compose.yml 	modified:   mqttSub/src/main.py 	modified:   mqttSub/src/mqttClient.py

--- mqttSub/src/main.py
+++ mqttSub/src/main.py
@@ -13,120 +13,37 @@ __status__ = "Development"
 # =============================================================================
 
 import json
-import random
-import re
-import string
 import sys
 import time
-# from datetime import datetime
 from readLogConfig import configure_logging_from_file
 from mqttClient import MQTTClient
 from readConfig import read_configuration
-
 
 # Constants
 CONFIG_FILE_PATH = '/app/data/conf.json'
 
 # ==============================================================================
-# Function to sanitize topic components
-# ==============================================================================
-
-
-def sanitize_topic_component(component):
-    """
-    Sanitizes the topic component to ensure it contains no uppercase letters,
-    spaces, or special characters, replacing them with underscores.
-
-    :param component: A string representing a single component of the
-                      MQTT topic.
-    :return: A sanitized string with only lowercase letters, numbers,
-             and underscores.
-    """
-    component = component.lower()
-    component = re.sub(r'[^a-z0-9_]+', '_', component)
-    return component
-
-# ==============================================================================
-# Function to safely generate a random message value
-# ==============================================================================
-
-
-def evaluate_message_value(item):
-    """
-    Generate a random or default value for the message based on its type and
-    constraints. If the type is not recognized, return an error message as the
-    value.
-
-    :param item: Dict containing the message definition including type and
-                 constraints.
-    :return: Value for the message, or an error string if type is unrecognized.
-    """
-    value_type = item.get('type')
-    try:
-        if value_type == 'integer':
-            return random.randint(item.get('min', 0), item.get('max', 100))
-        elif value_type == 'string':
-            return item.get('default', ''.join(random.choices(
-                string.ascii_uppercase + string.digits, k=10)))
-        elif value_type == 'boolean':
-            return random.choice([True, False])
-        elif value_type == 'enum':
-            return random.choice(item['enum'])
-        elif value_type == 'float':
-            min_val = item.get('min', 0.0)
-            max_val = item.get('max', 100.0)
-            return round(random.uniform(min_val, max_val),
-                         item.get('precision', 2))
-        else:
-            return "Error: Unrecognized type"
-    except (KeyError, ValueError, TypeError) as e:
-        return f"Error: {e}"
-
-# ==============================================================================
-# Function to build a JSON-formatted payload for MQTT messages
-# ==============================================================================
-
-
-def build_topic_payload(variable, value, unit):
-    """
-    Constructs a JSON-formatted payload for an MQTT message including a
-    timestamp and the given variable information.
-
-    :param variable: The name of the variable.
-    :param value: The value to send.
-    :param unit: The unit of the value.
-    :return: A JSON-formatted string payload.
-    """
-    payload = {
-        # 'timestamp': int(time.time()),
-        # 'timestamp_readable': datetime.utcnow().strftime(
-        #     "%d-%m-%Y %H:%M:%S.%f")[:-3] + "Z",
-        'variable': variable,
-        'value': value,
-        'unit': unit,
-    }
-    return json.dumps(payload)
-
-# ==============================================================================
 # Main function
 # ==============================================================================
 
+def on_message(client, userdata, message):
+    """ Callback function for when a message is received. """
+    payload = str(message.payload.decode("utf-8"))
+    logger.info(f"Message received: topic='{message.topic}' payload='{payload}'")
 
 def main():
-    """ Main function for publishing MQTT messages. """
+    """ Main function for subscribing to MQTT topics. """
     logger = configure_logging_from_file()
 
     # Load and validate configuration
     try:
         config_data = read_configuration(CONFIG_FILE_PATH)
     except json.JSONDecodeError as e:
-        logger.error("Invalid JSON format in config: %s - %s",
-                     CONFIG_FILE_PATH, e)
+        logger.error("Invalid JSON format in config: %s - %s", CONFIG_FILE_PATH, e)
         sys.exit(1)
 
     if config_data is None:
-        logger.error("Failed to read config: %s. Exiting...",
-                     CONFIG_FILE_PATH)
+        logger.error("Failed to read config: %s. Exiting...", CONFIG_FILE_PATH)
         sys.exit(1)
 
     # MQTT configuration
@@ -140,25 +57,14 @@ def main():
     # Subscriber configuration
     subscriber_config = config_data.get("SUBSCRIBER", {})
     SUBSCRIBER_NAME = subscriber_config.get("NAME", "default_subscriber")
-    SUBSCRIBER_DESCRIPTION = subscriber_config.get("DESCRIPTION",
-                                                   "Default MQTT Subscriber")
+    SUBSCRIBER_DESCRIPTION = subscriber_config.get("DESCRIPTION", "Default MQTT Subscriber")
 
-    logger.info("MQTT Configuration:")
-    logger.info(f"Host: {MQTT_HOST}")
-    logger.info(f"Port: {MQTT_PORT}")
-    logger.info(f"SSL Enabled: {MQTT_ENABLE_SSL}")
-    logger.info(f"User: {MQTT_USER}")
-    logger.info("Password: [HIDDEN]")
+    # Topics to subscribe
+    topics = config_data.get("TOPICS", [])
+
+    logger.info("Starting MQTT Subscriber...")
     logger.info(f"Subscriber Name: {SUBSCRIBER_NAME}")
     logger.info(f"Subscriber Description: {SUBSCRIBER_DESCRIPTION}")
-
-    # Topic construction
-    topic_components = [
-        sanitize_topic_component(comp) for comp in config_data.get(
-            "TOPIC_STRUCTURE", [])
-    ]
-    TOPIC = '/'.join(topic_components)
-    logger.info(f"Sanitized Full Topic Path: {TOPIC}")
 
     mqtt_client = MQTTClient(
         mqtt_host=MQTT_HOST,
@@ -168,38 +74,30 @@ def main():
         mqtt_password=MQTT_PASSWORD,
         subscriber_name=SUBSCRIBER_NAME)
 
+    # Set the on_message callback and connect to broker
+    mqtt_client.on_message = on_message
     mqtt_client.connect()
 
     while not mqtt_client.flag_connected:
         logger.info("Waiting for MQTT client to connect...")
         time.sleep(1)
 
-    # Message processing
-    messages_config = config_data.get('MESSAGES', [])
+    # Subscribing to topics
+    for topic_info in topics:
+        topic = topic_info["TOPIC"]
+        qos = topic_info.get("qos", 0)
+        logger.info(f"Subscribing to topic: {topic} with QoS {qos}")
+        mqtt_client.subscribe(topic, qos)
+
+    # Start the network loop
     try:
-        while True:
-            for message in messages_config:
-                message_name = message['name']
-                message_topic = f"{TOPIC}/{message_name}"
-                value = evaluate_message_value(message)
-                unit = message.get('unit', '')
-                payload = build_topic_payload(message_name, value, unit)
-
-                logger.debug(f"Publishing to {message_topic}: {payload}")
-                mqtt_client.publish(topic=message_topic, payload=payload,
-                                    qos=0, retain=False)
-
-                # time.sleep(1)
-
-            if not mqtt_client.flag_connected:
-                logger.error("Lost connection to broker. Exiting...")
-                break
-
+        mqtt_client.loop_forever()
     except KeyboardInterrupt:
         logger.info("Keyboard interrupt detected. Exiting...")
     except Exception as e:
         logger.error("An error occurred: %s", e)
-
+    finally:
+        mqtt_client.disconnect()
 
 if __name__ == "__main__":
     main()
--- mqttSub/src/mqttClient.py
+++ mqttSub/src/mqttClient.py
@@ -1,120 +1,101 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
-import paho.mqtt.client as mqttPubSub
-import time
+import paho.mqtt.client as mqtt
 import logging
 
 
 class MQTTClient:
-    def __init__(
-            self,
-            mqtt_host,
-            mqtt_port,
-            mqtt_enable_ssl,
-            mqtt_user,
-            mqtt_password,
-            subscriber_name):
+    def __init__(self,
+                 mqtt_host,
+                 mqtt_port,
+                 mqtt_enable_ssl,
+                 mqtt_user,
+                 mqtt_password,
+                 subscriber_name):
         self.mqtt_host = mqtt_host
         self.mqtt_port = mqtt_port
         self.mqtt_enable_ssl = mqtt_enable_ssl
         self.mqtt_user = mqtt_user
         self.mqtt_password = mqtt_password
-        self.client = mqttPubSub.Client(
-            client_id=subscriber_name,
-            userdata=None,
-            protocol=mqttPubSub.MQTTv5)
+        self.client = mqtt.Client(client_id=subscriber_name, 
+                                  clean_session=True)
         self.flag_connected = False
+
+        # Set the callback functions
+        self.client.on_connect = self.on_connect
+        self.client.on_disconnect = self.on_disconnect
+        self.client.on_subscribe = self.on_subscribe
+        self.client.on_message = self.on_message
+        self.client.on_log = self.on_log
+
+        if self.mqtt_enable_ssl:
+            self.client.tls_set()  # Add arguments if necessary (certs, keys, etc.)
+
+        self.client.username_pw_set(self.mqtt_user, self.mqtt_password)
 
     def connect(self):
         try:
-            if self.mqtt_enable_ssl:
-                self.client.tls_set(tls_version=mqttPubSub.ssl.PROTOCOL_TLS)
-                self.client.username_pw_set(self.mqtt_user, self.mqtt_password)
-
-            # Set the callback functions
-            self.client.on_connect = self.on_connect
-            self.client.on_disconnect = self.on_disconnect
-            self.client.on_subscribe = self.on_subscribe
-            self.client.on_message = self.on_message
-            self.client.on_log = self.on_log
-
             # Connect to the MQTT broker
             self.client.connect(self.mqtt_host, self.mqtt_port)
-            logging.info(
-                'Connecting to MQTT broker: %s on port: %s',
-                self.mqtt_host,
-                self.mqtt_port)
+            logging.info('Connecting to MQTT broker: %s on port: %s',
+                         self.mqtt_host, self.mqtt_port)
             self.client.loop_start()
 
         except Exception as e:
             logging.error(f"Error connecting to MQTT broker: {str(e)}")
-            # Retry connection after a delay
-            time.sleep(10)
-            self.connect()  # Retry connection
+            # Retry connection or handle reconnection logic here
 
-    def on_connect(self, client, userdata, flags, rc, properties=None):
-        """ callback on connect """
+    def on_connect(self, client, userdata, flags, rc):
+        """ Callback on connect """
         if rc == 0:
             self.flag_connected = True
-            logging.info(
-                'Connected successfully to MQTT host: %s on port: %s',
-                self.mqtt_host,
-                self.mqtt_port)
+            logging.info('Connected successfully to MQTT host: %s on port: %s', 
+                        self.mqtt_host, self.mqtt_port)
         else:
             self.flag_connected = False
-            logging.error(
-                'No connection to MQTT host: %s on port: %s,'
-                ' returned result code: %s',
-                self.mqtt_host,
-                self.mqtt_port,
-                str(rc))
+            logging.error('Failed to connect to MQTT host: %s on port: %s, result code: %s', 
+                          self.mqtt_host, self.mqtt_port, rc)
 
     def on_disconnect(self, client, userdata, rc):
-        """ callback on disconnect """
-        logging.info(
-            'Disconnected from MQTT host: %s on port: %s with result code: %s',
-            self.mqtt_host,
-            self.mqtt_port,
-            str(rc))
+        """ Callback on disconnect """
+        self.flag_connected = False
+        logging.info('Disconnected from MQTT host: %s on port: %s with result code: %s', 
+                     self.mqtt_host, self.mqtt_port, rc)
         client.loop_stop()
 
-    def on_subscribe(
-            self,
-            client,
-            userdata,
-            mid,
-            granted_qos,
-            properties=None):
-        """ callback on subscribe """
-        logging.info("Subscribed: %s %s", str(mid), str(granted_qos))
+    def on_subscribe(self, client, userdata, mid, granted_qos):
+        """ Callback on subscribe """
+        logging.info("Subscribed: mid=%s qos=%s", str(mid), str(granted_qos))
 
     def on_message(self, client, userdata, msg):
-        """ callback on message for when a PUBLISH message is received
-        from the server """
-        payload = str(msg.payload.decode("utf-8"))
-        logging.info(
-            'Received topic: %s payload: %s qos: %s retain: %s',
-            msg.topic,
-            payload,
-            msg.qos,
-            msg.retain)
+        """ Callback on message for when a PUBLISH message is received from the server """
+        logging.info('Received message on topic: %s with QoS %s: %s',
+                     msg.topic,
+                     msg.qos,
+                     msg.payload.decode("utf-8"))
 
     def on_log(self, client, userdata, level, buf):
-        """ callback for logging """
+        """ Callback for logging """
         logging.debug('%s', buf)
 
+    def subscribe(self, topic, qos=0):
+        """ Subscribe to a topic on the MQTT broker. """
+        if self.flag_connected:
+            self.client.subscribe(topic, qos)
+        else:
+            logging.error("""Cannot subscribe because the client is
+                           not connected.""")
+
     def publish(self, topic, payload, qos=0, retain=False):
-        """
-        Publish a message to a topic on the MQTT broker.
-        :param topic: The topic to publish to.
-        :param payload: The message payload to publish.
-        :param qos: The Quality of Service level of the message.
-        :param retain: If True, the message will be set as the
-                        "last known good" for the topic.
-        """
-        if self.flag_connected:  # Only attempt to publish if connected
+        """ Publish a message to a topic on the MQTT broker. """
+        if self.flag_connected:
             self.client.publish(topic, payload, qos, retain)
         else:
-            logging.error("Cannot publish because the client is not "
-                          "connected.")
+            logging.error("""Cannot publish because the client is not
+                           connected.""")
+
+    def disconnect(self):
+        """ Disconnect from the MQTT broker. """
+        self.client.disconnect()
+        
